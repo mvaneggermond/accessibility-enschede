06f9149291226fa084ba8dabc7ad4b8de81bba8b
van Eggermond Michael <michael.vaneggermond@fhnw.ch>
2022-06-16 20:12:46 +0200

    Update the max walking distance

--- 01_accessibility_calculation.R
+++ 01_accessibility_calculation.R
@@ -31,11 +31,17 @@ library(wesanderson)
 # I pre-built the network and uploaded it
 path <- "_data"
 # You can see the data sets that we have below
+# E-mail me for the data!
+# CBS 100 meter data for 2020 is available here:
+# https://www.cbs.nl/nl-nl/dossier/nederland-regionaal/geografische-data/kaart-van-100-meter-bij-100-meter-met-statistieken
+# The pt schedule is available here:
+# https://gtfs.ovapi.nl/nl/
+# I merged the following pbf files from geofabrik with osmium
+# osmium merge flevoland-latest.osm.pbf gelderland-latest.osm.pbf groningen-latest.osm.pbf noord-brabant-latest.osm.pbf overijssel-latest.osm.pbf utrecht-latest.osm.pbf -o osm_merge_nl.pbf
 list.files(path)
 r5r_core <- r5r::setup_r5(data_path = path, verbose = TRUE)
 # This packages comes with several functions
 # First let's look at itineraries
-?detailed_itineraries
 # Test data
 df_test_destinations <- data.frame(id= numeric(), lon= numeric(), lat = numeric())
 
@@ -119,7 +125,7 @@ tail(df_population %>% relocate(lon,lat,.after="crs28992res100m"))
 # Example inspired by https://ipeagit.github.io/r5r/articles/calculating_isochrones.html
 # routing inputs
 mode <- c("WALK", "TRANSIT")
-max_walk_dist <- 500 # in meters
+max_walk_dist <- 800 # in meters
 travel_time_cutoff <- 120 # in minutes
 time_window <- 60 # in minutes
 percentiles <- 50
@@ -138,18 +144,63 @@ ttm <- travel_time_matrix(r5r_core,
 ttm
 sf_output <- st_read("_data/cbs_vk100_2020_v2.gpkg",layer='vierkant_100m_2020')
 
-sf_output <- sf_output %>% mutate(id=crs28992res100m) %>%  inner_join(ttm ,by=c("id"="toId"))
-write_sf(sf_output,"travel_time_matrix.gpkg")
+sf_output2 <- sf_output %>% mutate(id=crs28992res100m) %>%  inner_join(ttm ,by=c("id"="toId"))
+write_sf(sf_output2,"travel_time_matrix.gpkg")
 
 # add coordinates of destinations to travel time matrix
 ttm <- df_population %>% 
   mutate(id=as.character(id)) %>%
   inner_join(ttm ,by=c("id"="toId"))
 
+write_csv(ttm,"ttm_enschede_transit.csv")
 
+
+ttm_bike <- travel_time_matrix(r5r_core,
+                          origins = sf_test_destinations[1,],
+                          destinations = df_population,
+                          mode = c('BICYCLE'),
+                          departure_datetime = departure_datetime,
+                          max_walk_dist = 800,
+                          max_trip_duration = 120,
+                          time_window = time_window,
+                          bike_speed=12,
+                          max_lts = 3,
+                          
+                          percentiles = percentiles,
+                          progress = FALSE)
+# View the results
+ttm_bike
+
+ttm_ebike <- travel_time_matrix(r5r_core,
+                               origins = sf_test_destinations[1,],
+                               destinations = df_population,
+                               mode = c('BICYCLE'),
+                               departure_datetime = departure_datetime,
+                               max_walk_dist = 800,
+                               max_trip_duration = 120,
+                               time_window = time_window,
+                               bike_speed=25,
+                               max_lts = 3,
+                               
+                               percentiles = percentiles,
+                               progress = FALSE)
+# View the results
+ttm_ebike
+?travel_time_matrix
+ttm_all <- ttm_ebike %>% dplyr::rename(travel_time_ebike = travel_time) %>%
+  left_join(ttm_bike %>%dplyr::rename(travel_time_bike = travel_time) ) %>%
+  left_join(ttm  %>%dplyr::rename(travel_time_pt = travel_time))%>%
+  mutate(perc_diff=(travel_time_pt-travel_time_ebike)/travel_time_ebike)%>%
+  mutate(perc_diff_100=round(100*perc_diff,0))
   
 
-write_csv(ttm,"ttm_enschede_transit.csv")
+sf_output3 <- sf_output %>% mutate(id=crs28992res100m) %>%  inner_join(ttm_all ,by=c("id"="toId"))%>%
+  mutate(aantal_inwoners=if_else(aantal_inwoners<0,0.0,aantal_inwoners*1.0))
+write_sf(sf_output3,"travel_time_matrix_diff.gpkg")
+
+  View(sf_output3)
+  
+
 
 # interpolate estimates to get spatially smooth result
 travel_times.interp <- with(na.omit(ttm), interp(lon, lat, travel_time,duplicate=T)) %>%
@@ -184,10 +235,10 @@ ggplot(site) +
 # Accessibilitly ------
 # routing inputs
 mode <- c("WALK", "TRANSIT")
-max_walk_dist <- 1000 # in meters
+max_walk_dist <- 1200 # in meters
 travel_time_cutoff <- 45 # in minutes
 time_window <- 60 # in minutes
-percentiles <- 50
+percentiles <- 25
 departure_datetime <- as.POSIXct("16-06-2022 07:00:43",
                                  format = "%d-%m-%Y %H:%M:%S",
                                  tz = "CET")
@@ -216,8 +267,8 @@ access_car
 access_pt <- accessibility(r5r_core,
                         origins = sf_test_destinations,
                         destinations = df_population,
-                        mode = mode,
-                        opportunities_colname = "aantal_inwoners_25_tot_45_jaar",
+                        mode = c('WALK','TRANSIT'),
+                        opportunities_colname = "aantal_inwoners",
                         decay_function = "step",
                         cutoffs = travel_time_cutoff,
                         departure_datetime = departure_datetime,
@@ -233,13 +284,14 @@ access_bicycle <- accessibility(r5r_core,
                         origins = sf_test_destinations,
                         destinations = df_population,
                         mode = c('BICYCLE'),
-                        opportunities_colname = "aantal_inwoners_25_tot_45_jaar",
+                        opportunities_colname = "aantal_inwoners",
                         decay_function = "step",
                         cutoffs = travel_time_cutoff,
                         departure_datetime = departure_datetime,
                         max_walk_dist = max_walk_dist,
                         time_window = time_window,
                         bike_speed=14,
+                        max_lts=3,
                         percentiles = percentiles,
                         verbose = FALSE)
 
@@ -251,13 +303,14 @@ access_ebike <- accessibility(r5r_core,
                                 origins = sf_test_destinations,
                                 destinations = df_population,
                                 mode = c('BICYCLE'),
-                                opportunities_colname = "aantal_inwoners_25_tot_45_jaar",
-                                decay_function = "step",
+                              opportunities_colname = "aantal_inwoners",
+                              decay_function = "step",
                                 cutoffs = travel_time_cutoff,
                                 departure_datetime = departure_datetime,
                                 max_walk_dist = max_walk_dist,
                                 time_window = time_window,
                                 bike_speed=25,
+                                max_lts=3,
                                 percentiles = percentiles,
                                 verbose = FALSE)
 
